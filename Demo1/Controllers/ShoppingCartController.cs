9ea39939fc20117a262836fca993a021b0e415ec
nhutoan <nhutoan399@gmail.com>
2022-05-10 16:22:05 +0700

    added cart

---
+++ Demo1/Controllers/ShoppingCartController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo1.Models;
+
+namespace Demo1.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        ProductDbContext _db = new ProductDbContext();
+        // GET: ShoppingCart
+        public Cart GetCart()
+        {
+            Cart cart = Session["Cart"] as Cart;
+            if(cart == null || Session["Cart"] == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public ActionResult AddToCart(int? id)
+        {
+            var p = _db.ProductMasters.SingleOrDefault(s => s.BookId == id);
+            if(p != null)
+            {
+                GetCart().Add(p);
+            }
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+        }
+        public ActionResult ShowToCart(int? id)
+        {
+            if (Session["Cart"] == null)
+                return RedirectToAction("IndexUser", "Product");
+            Cart cart = Session["Cart"] as Cart;
+            return View(cart);
+        }
+        public ActionResult Update_Quantity_Cart(FormCollection form)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            int id_pro = int.Parse(form["Book_Id"]);
+            int quantity = int.Parse(form["Quantity"]);
+            cart.Update_Quantity_Shopping(id_pro, quantity);
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+        }
+        public ActionResult RemoveCart(int id)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            cart.Remove_CartItem(id);
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+        }
+
+    }
+}
---
+++ Demo1/Models/Cart.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Demo1.Models;
+
+namespace Demo1.Models
+{
+    //item hang
+    public class CartItem
+    {
+        public ProductMaster _shopping_product { get; set; }
+        public int _shopping_quantity { get; set; }
+    }
+    //gio hang
+    public class Cart
+    {
+        List<CartItem> items = new List<CartItem>();
+        public IEnumerable<CartItem> Items
+        {
+            get { return items; }
+        }
+        public void Add(ProductMaster p, int _quantity = 1)
+        {
+            var item = items.FirstOrDefault(s=>s._shopping_product.BookId==p.BookId);
+            if (item == null)
+            {
+                items.Add(new CartItem
+                {
+                    _shopping_product = p,
+                    _shopping_quantity = _quantity
+                });
+            }
+            else
+            {
+                item._shopping_quantity += _quantity;
+            }
+        }
+        public void Update_Quantity_Shopping(int id, int _quantity)
+        {
+            var item = items.Find(s => s._shopping_product.BookId == id);
+            if (item != null)
+            {
+                item._shopping_quantity = _quantity;
+            }
+        }
+        public double Total_Money()
+        {
+            var total = items.Sum(s => s._shopping_product.BookPrice * s._shopping_quantity);
+            return (double)total;
+        }
+        public void Remove_CartItem(int id)
+        {
+            items.RemoveAll(s => s._shopping_product.BookId == id);
+        }
+        
+    }
+}
